674203636cb220798676a5a41f76d48677b18267
Per Jansson <per.jansson@smartrefill.se>
2022-08-08 15:46:34 +0200

    aa

--- app/src/main/java/com/example/skiptraffi/MainActivity.kt
+++ app/src/main/java/com/example/skiptraffi/MainActivity.kt
@@ -5,24 +5,13 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
@@ -31,9 +20,7 @@ import com.example.skiptraffi.ui.theme.SkipTraffiTheme
 import com.example.skiptraffi.util.Constants.LATITUDE_KEY
 import com.example.skiptraffi.util.Constants.LONGITUDE_KEY
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.tasks.CancellationTokenSource
 
 class MainActivity : ComponentActivity() {
 
@@ -63,38 +50,18 @@ class MainActivity : ComponentActivity() {
             e.printStackTrace()
         }
 
-        val priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
-        val cancellationTokenSource = CancellationTokenSource()
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
-        fusedLocationClient.getCurrentLocation(priority, cancellationTokenSource.token)
-            .addOnSuccessListener { location ->
-                Log.d("Location", "location is found: $location")
-            }
-            .addOnFailureListener { exception ->
-                Log.d("Location", "Oops location failed with exception: $exception")
-            }
-
-     /*
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         fusedLocationClient.lastLocation
             .addOnSuccessListener { location: Location? ->
                 // Got last known location. In some rare situations this can be null.
-                LONGITUDE_KEY = location?.longitude ?: 00.00
-                LATITUDE_KEY = location?.latitude ?: 00.00
-               //   Log.d("TestHej", "longitude: " + location?.longitude)
-               //   Log.d("TestHej", "latitude: " + location?.latitude)
+                //LONGITUDE_KEY = location?.longitude ?: 00.00
+                //LATITUDE_KEY = location?.latitude ?: 00.00
+                LONGITUDE_KEY = 60.0
+                LATITUDE_KEY = 18.0
             }
-      */
 
         setContent {
-
-
             SkipTraffiTheme {
-
-
-
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -103,7 +70,6 @@ class MainActivity : ComponentActivity() {
                     navController = rememberNavController()
                     SetupNavGraph(navController = navController)
                 }
-
             }
         }
     }
--- app/src/main/java/com/example/skiptraffi/util/Constants.kt
+++ app/src/main/java/com/example/skiptraffi/util/Constants.kt
@@ -3,6 +3,8 @@ package com.example.skiptraffi.util
 object Constants {
     var DETAIL_CITY_KEY = "cityName"
 
-    var LONGITUDE_KEY = 00.00
-    var LATITUDE_KEY = 00.00
+    var LONGITUDE_KEY = 18.0
+    var LATITUDE_KEY = 60.0
+
+    Här är felet, tar ut fel long och alt
 }
